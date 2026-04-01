171d82cf385b80d23858b13490bacdad8f8263ce
Mridani Pandey <mridanipandey8813@gmail.com>
2026-04-01 18:18:33 +0545

    fix:remove file imports for prisma

--- app/api/inngest/function.js
+++ app/api/inngest/function.js
@@ -1,5 +1,5 @@
 import {inngest} from "../../../inngest/client";
-import prisma from "../../../lib/prisma";
+import {prisma} from "../../../lib/prisma";
 
 //Inngest fn to save user data to db
 export const syncUserCreation = inngest.createFunction(
